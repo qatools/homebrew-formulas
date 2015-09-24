97c3c7997957b04c874045572c774faec1119854
Dmitry Baev <charlie@yandex-team.ru>
2015-09-24 15:28:36 +0300

    make it works

--- Formula/allure-commandline.rb
+++ Formula/allure-commandline.rb
@@ -1,7 +1,8 @@
 require 'formula'
 
 class AllureCommandline < Formula
-  homepage 'https://github.com/allure-framework/'
+  desc 'Allure Commandline client'
+  homepage 'http://allure.qatools.ru/'
   url 'https://github.com/allure-framework/allure-core/releases/download/allure-core-1.4.18/allure-commandline.zip'
   head 'https://github.com/allure-framework/allure-core.git'
   sha1 '013c6a69bb67dbd9696b0b7c1c8ccb33c4725850'
@@ -9,9 +10,10 @@ class AllureCommandline < Formula
   conflicts_with "allure-cli", :because => "the old version of Allure commandline was installed"
 
   def install
-    jarname = 'allure-commandline.jar'
-    libexec.install "lib/#{jarname}"
-    bin.write_jar_script libexec/jarname, "allure"
+    # Remove windows files
+    rm_rf Dir["bin/*.bat"]
+    libexec.install Dir["*"]
+    bin.write_exec_script libexec/"bin/allure"
   end
 
   test do
