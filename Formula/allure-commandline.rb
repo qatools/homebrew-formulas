d5997fb952cf70eb2a83d65448d7084701642194
Dmitry Baev <baev.dm@gmail.com>
2015-09-25 20:49:17 +0300

    update to 1.4.19

--- Formula/allure-commandline.rb
+++ Formula/allure-commandline.rb
@@ -6,7 +6,7 @@ class AllureCommandline < Formula
   url 'https://github.com/allure-framework/allure-core/releases/download/allure-core-1.4.19/allure-commandline.zip'
   head 'https://github.com/allure-framework/allure-core.git'
   sha1 'd31556f484e082423089c8454d41ab4375c7ef4b'
-  version "1.4.18"
+  version "1.4.19"
   conflicts_with "allure-cli", :because => "the old version of Allure commandline was installed"
 
   def install
