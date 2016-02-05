382d54d61e050094215b570d814755ea3037b261
Dmitry Baev <baev.dm@gmail.com>
2016-02-05 19:15:17 +0300

    update to 1.4.22

--- Formula/allure-commandline.rb
+++ Formula/allure-commandline.rb
@@ -3,10 +3,10 @@ require 'formula'
 class AllureCommandline < Formula
   desc 'Allure Commandline client'
   homepage 'http://allure.qatools.ru/'
-  url 'https://github.com/allure-framework/allure-core/releases/download/allure-core-1.4.20/allure-commandline.zip'
+  url 'https://github.com/allure-framework/allure-core/releases/download/allure-core-1.4.22/allure-commandline.zip'
   head 'https://github.com/allure-framework/allure-core.git'
-  sha1 '7af28014db57c5eaaba3a328fd0a4b7ec0f4b55a'
-  version "1.4.20"
+  sha1 'c90c0fe84d9ae0e4f87be864ce3359ac11a6a0a7'
+  version "1.4.22"
 
   def install
     # Remove windows files
